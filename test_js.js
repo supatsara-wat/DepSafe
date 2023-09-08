d0e26fc1a844d0b4bc2f203ff29fabff19ca5dcb
Supatsara Wattanakriengkrai <supatsara.wat@gmail.com>
2023-09-08 15:57:36 +0900

    Create test_js.js

---
+++ test_js.js
@@ -0,0 +1,12 @@
+    let combineMessage = [];
+        combineMessage.push('Please be aware!!')
+        if (found_packageJson === true) {
+            combineMessage.push(`${diffData.additions} changes have been made to [ package.json ] file`)
+        }
+
+        if (changedJSfiles.length >= 1) {
+            joinText = changedJSfiles.join('\n')
+            combineMessage.push(` 
+            ${countChangedLines} changes have been made to [ require() ] in .js file:  \n${joinText} 
+           `)
+        }
