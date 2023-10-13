ca6879383e6df6da871ade0e84ebd998aa2ecf23
Supatsara Wattanakriengkrai <supatsara.wat@gmail.com>
2023-10-13 17:50:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const github = require('@actions/github2');
 
 function parsePatch(patch) {
     const added = [];
