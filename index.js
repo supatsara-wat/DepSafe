976df2f59645468d7fe5b96a274f6584e87fcb87
iwsc2022 <97493842+iwsc2022@users.noreply.github.com>
2023-10-17 15:30:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const helloWorldModule = require('./module.js');
 
 function parsePatch(patch) {
     const added = [];
